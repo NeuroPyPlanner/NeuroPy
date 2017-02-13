392b1417a424a807c2ce94bc9d8f8c35320e834f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-13 12:25:45 -0800

    creating todo model

--- neuropy/todo/models.py
+++ neuropy/todo/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+"""Model for users todos."""
 
-# Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from userprofile.models import UserProfile
+
+
+@python_2_unicode_compatible
+class Todo(models.Model):
+    """Model for an individual Todo."""
+
+    PRIORITY_CHOICES = (
+        (1, 'Now'),
+        (2, 'Urgent'),
+        (3, 'Semi Urgent'),
+        (4, 'Non Urgent'),
+    )
+
+    EASE_CHOICES = (
+        (1, 'Easy'),
+        (2, 'Medium'),
+        (3, 'Difficult'),
+    )
+
+    self.title = models.CharField()
+    self.description =
+    self.date = 
+    self.duration = models.IntegerField(blank=True, null=True)
+    self.ease = models.IntegerField(choices=EASE_CHOICES)
+    self.priority = models.IntegerField(choices=PRIORITY_CHOICES)
+    self.owner =  models.ForeignKey(UserProfile,
+                                     related_name='todo',
+                                     blank=True,
+                                     null=True
+                                     )
+
+    def __str__(self):
+        """String representation of Todo."""
+        return self.title
