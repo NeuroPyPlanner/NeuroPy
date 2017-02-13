66612d077d7d7c05a8a4abe0d2ac5ffd78e54b82
pasaunders <kaorti@gmail.com>
2017-02-13 11:27:32 -0800

    begin building user proflie model

--- neuropy/userprofile/models.py
+++ neuropy/userprofile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+@python_2_unicode_compatable
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    active
