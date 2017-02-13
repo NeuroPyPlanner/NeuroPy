da87be8ee24dc698eb570e715dfa3733e71b1f9b
Amos Boldor <amosboldor@gmail.com>
2017-02-13 11:27:17 -0800

    add registration templates and routes

--- neuropy/neuropy/urls.py
+++ neuropy/neuropy/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib import admin, auth
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^login/', auth.views.login, name='login'),
+    url(r'^logout/', auth.views.logout, {'next_page': '/'}, name='logout'),
 ]
