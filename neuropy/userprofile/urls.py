918473f9894aa8d53bbbba0689e094ebaf38b9a6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-14 11:11:22 -0800

    removed login decorator

--- neuropy/userprofile/urls.py
+++ neuropy/userprofile/urls.py
@@ -1,9 +1,8 @@
 """Profile urls."""
 from django.conf.urls import url
 from .views import ProfileView, EditProfile
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^edit/$', login_required(EditProfile.as_view()), name='edit-profile'),
-    url(r'^$', login_required(ProfileView.as_view()), name='profile')
+    url(r'^edit/$', EditProfile.as_view(), name='edit-profile'),
+    url(r'^$', ProfileView.as_view(), name='profile')
 ]
