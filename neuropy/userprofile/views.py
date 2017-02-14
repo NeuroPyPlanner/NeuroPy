c28c45413d6814847a66fc41a5650977f6672b72
Amos Boldor <amosboldor@gmail.com>
2017-02-13 16:31:57 -0800

    made possiable for the user to edit the user info also besides profile

--- neuropy/userprofile/views.py
+++ neuropy/userprofile/views.py
@@ -1,6 +1,9 @@
+"""Views for profile."""
 from django.views.generic import detail, UpdateView
 from userprofile.models import Profile
 from django.urls import reverse_lazy
+from userprofile.forms import ProfileForm
+from django.http import HttpResponseRedirect
 
 
 class ProfileView(detail.DetailView):
@@ -20,14 +23,19 @@ class EditProfile(UpdateView):
 
     template_name = 'userprofile/edit_profile.html'
     model = Profile
-    fields = [
-        'active_period_start',
-        'active_period_end',
-        'peak_period',
-        'dose_time'
-    ]
+    form_class = ProfileForm
     success_url = reverse_lazy('profile')
 
     def get_object(self):
         """Return logged in users profile."""
         return self.request.user.profile
+
+    def form_valid(self, form):
+        """Save object after post."""
+        self.object = form.save()
+        self.object.user.first_name = form.cleaned_data['First Name']
+        self.object.user.last_name = form.cleaned_data['Last Name']
+        self.object.user.email = form.cleaned_data['Email']
+        self.object.user.save()
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
