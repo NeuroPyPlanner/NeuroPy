dec9ec72f6b42e26ef5c70cab3b331dcb0e50cd2
Your Name <you@example.com>
2017-02-14 11:09:36 -0800

    added more tests, using factoryboy to save to test db insetad. All passing.

--- neuropy/medication/tests.py
+++ neuropy/medication/tests.py
@@ -2,50 +2,64 @@
 
 from django.test import TestCase
 from medication.models import Medication
+import factory
 import datetime
+
+
+class MedicationFactory(factory.django.DjangoModelFactory):
+    """Create test instance of todos."""
+
+    class Meta:
+        """Invoke Todo instance using Todo model class."""
+
+        model = Medication
+
+    name = 'CONCERTA'
+    med_type = 'stimulant'
+    treating_dis = 'ADD/ADHD'
+    half_life = datetime.timedelta(hours=3, minutes=30)  # 03:30:00
+    ramp_up = datetime.timedelta(hours=4, minutes=30)  # 04:30:00
+    peak_period = datetime.timedelta(hours=7)  # 07:00:00
 
 
 class MedicationTestCase(TestCase):
     """Test the Medication model."""
 
-    def medication_instance(self):
-        """Create An Instance of a Medication and Save to DB."""
-        name = 'CONCERTA'
-        med_type = 'stimulant'
-        treating_dis = 'ADD/ADHD'
-        half_life = datetime.timedelta(hours=3, minutes=30)  # 03:30:00
-        ramp_up = datetime.timedelta(hours=4, minutes=30)  # 04:30:00
-        peak_period = datetime.timedelta(hours=7)  # 07:00:00
-
-        objects = Medication.objects.create(
-            name=name,
-            med_type=med_type,
-            treating_dis=treating_dis,
-            half_life=half_life,
-            ramp_up=ramp_up,
-            peak_period=peak_period
-        )
-
-        objects.save()
-        return objects.id
+    def setUp(self):
+        """Setup for medications."""
+        self.medications = [MedicationFactory.create() for i in range(10)]
 
     def test_name(self):
         """Test that a medication instance has a name."""
-        objects = self.medication_instance()
-        medication = Medication.objects.get(id=objects)
+        medication = self.medications[1]
+        medication = Medication.objects.get(id=medication.id)
         self.assertTrue(medication.name == "CONCERTA")
+
+    def test_treating_disorder(self):
+        """Test that a medication instance has a name."""
+        medication = self.medications[1]
+        medication = Medication.objects.get(id=medication.id)
+        self.assertTrue(medication.treating_dis == "ADD/ADHD")
 
     def test_med_type(self):
         """Test medication instance type."""
-        objects = self.medication_instance()
-        medications = Medication.objects.filter(med_type="stimulant")
-        for medication in medications:
-            assert medication.med_type == "stimulant"
+        medication = self.medications[1]
+        medications = Medication.objects.get(id=medication.id)
+        self.assertTrue(medication.med_type == "stimulant")
+
+    def test_change_data(self):
+        """Test Editing medication data."""
+        medication = self.medications[1]
+        medication = Medication.objects.get(id=medication.id)
+        medication.peak_period = datetime.timedelta(hours=10)
+        medication.save()
+        medication = Medication.objects.get(id=medication.id)
+        self.assertTrue(medication.peak_period == datetime.timedelta(hours=10))
 
     def test_delete_med(self):
         """Test fetching medication after delete throws an exception."""
-        objects = self.medication_instance()
-        medication = Medication.objects.get(name="CONCERTA")
+        medication = self.medications[1]
+        medication = Medication.objects.get(id=medication.id)
         medication.delete()
         with self.assertRaises(Medication.DoesNotExist):
-            Medication.objects.get(name="CONCERTA")
+            Medication.objects.get(id=medication.id)
