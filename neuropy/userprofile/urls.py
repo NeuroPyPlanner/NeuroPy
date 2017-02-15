35b285a61cf99562e76ffb763bfa3866078b7cc7
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-15 11:58:47 -0800

    removed decorator that was causing an error

--- neuropy/userprofile/urls.py
+++ neuropy/userprofile/urls.py
@@ -5,5 +5,5 @@ from .views import ProfileView, EditProfile, ProfileFormView
 urlpatterns = [
     url(r'^edit/$', EditProfile.as_view(), name='edit-profile'),
     url(r'^$', ProfileView.as_view(), name='profile'),
-    url(r'submission_form/$', require_POST(ProfileFormView.as_view()), name='profile_form')
+    url(r'submission_form/$', ProfileFormView.as_view(), name='profile_form')
 ]
