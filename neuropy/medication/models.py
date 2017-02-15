431189bd5607f11517923647421a33c277386cf7
Your Name <you@example.com>
2017-02-15 14:21:55 -0800

    5 bucket json format.

--- neuropy/medication/models.py
+++ neuropy/medication/models.py
@@ -25,11 +25,15 @@ class Medication(models.Model):
     medium_end = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
     peak_start = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
     peak_end = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
+    post_peak_medium_start = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
+    post_peak_medium_end = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
+    post_peak_easy_start = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
+    post_peak_easy_end = models.CharField(validators=[validate_comma_separated_integer_list], max_length=50)
     # Ease_priority_matrix=models.CharField
 
     def __str__(self):
         """String representation of Medication."""
-        return "name: {},med_type: {}, treating_dis: {}, half_life: {}, ramp_up: {}, peak_period: {}, easy_start: {}, easy_end: {}, medium_start: {}, medium_end: {}, peak_start: {}, peak_end: {} ".format(
+        return "name: {},med_type: {}, treating_dis: {}, half_life: {}, ramp_up: {}, peak_period: {}, easy_start: {}, easy_end: {}, medium_start: {}, medium_end: {}, peak_start: {}, peak_end: {}, post_peak_medium_start: {}, post_peak_medium_end: {}, post_peak_easy_start: {}, post_peak_easy_end: {}".format(
             self.name,
             self.med_type,
             self.treating_dis,
@@ -42,4 +46,8 @@ class Medication(models.Model):
             self.medium_end,
             self.peak_start,
             self.peak_end,
+            self.post_peak_medium_start,
+            self.post_peak_medium_end,
+            self.post_peak_easy_start,
+            self.post_peak_medium_end
         )
